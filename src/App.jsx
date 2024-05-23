e31116c4bb7244b02440a55f97f0e0401b45b81f
Musharaf <105627926+musharaf-fleapo@users.noreply.github.com>
2024-05-23 20:34:21 +0530

    setting up project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import "./App.css";
+import { Routes } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
